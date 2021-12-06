c2e18e8da6bde01d277d9fb882f60dea4eccd22e
abdulrehman996 <abdurrehman9968@gmail.com>
2021-12-06 18:55:53 +0500

    prod grid view added

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:biz_link/models/product/product.dart';
 import 'package:biz_link/providers/product_provider.dart';
 import 'package:biz_link/screens/auth/login_page.dart';
 import 'package:biz_link/screens/product_screens/add_product_screen.dart';
+import 'package:biz_link/widgets/product/grid_view_of_prod.dart';
 import 'package:biz_link/widgets/product/product_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -36,24 +37,20 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.add),
-              title: Text('Add Product'),
-              subtitle: Text('Click here to add your product'),
-              onTap: () {
-                Navigator.of(context).pushNamed(AddProductScreen.routeName);
-              },
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) =>
-                    ProductTile(product: products[index]),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Add Product'),
+                subtitle: Text('Click here to add your product'),
+                onTap: () {
+                  Navigator.of(context).pushNamed(AddProductScreen.routeName);
+                },
               ),
-            )
-          ],
+              GridViewOfProducts(posts: products),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/product/grid_view_of_prod.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import '../../models/product/product.dart';
+import '../custom_widgets/custom_network_image.dart';
+
+class GridViewOfProducts extends StatelessWidget {
+  const GridViewOfProducts({
+    required this.posts,
+    this.isProfileWidget = true,
+    Key? key,
+  }) : super(key: key);
+  final List<Product> posts;
+  final bool isProfileWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 6,
+        mainAxisSpacing: 6,
+        childAspectRatio: 4 / 5,
+      ),
+      primary: false,
+      shrinkWrap: true,
+      itemCount: posts.length,
+      itemBuilder: (BuildContext context, int index) => ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8),
+          onTap: () {
+            // Navigator.of(context).push(
+            //   isProfileWidget
+            //       ? MaterialPageRoute<UserProductsScreen>(
+            //           builder: (_) => UserProductsScreen(
+            //               products: posts, selectedIndex: index),
+            //         )
+            //       : MaterialPageRoute<ProductDetailScreen>(
+            //           builder: (_) =>
+            //               ProductDetailScreen(product: posts[index]),
+            //         ),
+            // );
+          },
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .color!
+                      .withOpacity(0.1),
+                  offset: const Offset(0, 0),
+                  blurRadius: 1,
+                  spreadRadius: 3,
+                )
+              ],
+            ),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: CustomNetworkImage(
+                        imageURL: posts[index].prodURL[0].url,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            ' ${posts[index].price} - ${posts[index].title}',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                      ],
+                    ),
+                    Text(
+                      posts[index].price.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
